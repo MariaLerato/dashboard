e28ee6984590d551741dfa03a0f8c98db959aa0d
Moloto123 <70726456+Moloto123@users.noreply.github.com>
2022-02-22 14:57:20 +0200

    gth

--- src/Components/menu.jsx
+++ src/Components/menu.jsx
@@ -1,7 +1,7 @@
 <<<<<<< HEAD
 import React, { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-=======
+
 import React, { useState,useEffect } from "react";
 import { BrowserRouter, Routes, Route} from "react-router-dom";
 
